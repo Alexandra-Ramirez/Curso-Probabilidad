55aa748fb8b937d6cd60d3e0cfe990ef4a0685eb
Cesar Lara Avila <C-Lara@users.noreply.github.com>
2017-04-26 00:10:03 -0500

    Update tlc.R

--- Cotas-Teoria_limites/tlc.R
+++ Cotas-Teoria_limites/tlc.R
@@ -2,10 +2,11 @@
 
 library(animation)
 
-# un número positivo para configurar el intervalo de tiempo de la animación
+# un numero positivo para configurar el intervalo de tiempo de la animacion
 ani.options(interval = 0.5)
 par(mar = c(3, 3, 1, 0.5), mgp = c(1.5, 0.5, 0), tcl = -0.3)
 lambda = 4
+
 # Muestra aleatoria para la distribucion de Poisson con parametro lambda
 f <- function(n) rpois(n, lambda) 
 
@@ -13,3 +14,26 @@ f <- function(n) rpois(n, lambda)
 # del test de normalidad de Shapiro
 
 clt.ani(FUN = f, mean = lambda, sd = lambda)
+
+
+# El teorema del limite central y la distribucion Gaussiana
+
+#help(rug)
+central_limite<- function (r=runif, m=.5, s=1/sqrt(12), n=c(1,3,10,30), N=1000) {
+  for (i in n) {
+    x <- matrix(r(i*N),nc=i)
+    x <- ( apply(x, 1, sum) - i*m )/(sqrt(i)*s)
+    hist(x, col='light blue', probability=T, ylab = "Densidad", main=paste("n =",i), 
+         ylim=c(0,max(.4, density(x)$y)))
+    lines(density(x), col='red', lwd=3)
+    curve(dnorm(x), col='green', lwd=3, lty=3, add=T)
+    if( N>100 ) {
+      rug(sample(x,100))
+    } else {
+      rug(x)
+    }
+  }
+}
+op <- par(mfrow=c(2,2))
+central_limite()
+par(op)
